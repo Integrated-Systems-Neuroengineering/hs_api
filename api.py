58bdb98a31b3089acea8da13248710a6dba89915
Justin Frank <justin@pop-os.localdomain>
2022-04-12 16:53:22 -0700

    poetry is set up

--- api.py
+++ api.py
@@ -8,6 +8,7 @@ class CRI_network:
         self.inputs = inputs
         self.neuronModel = _simple_sim.map_neuron_type_to_int(neuronModel)
         self.threshold = threshold
+        self.simpleSim = None
         #we may need more attributes to track hardware implementation
 
     def update_synapse(self):
@@ -18,12 +19,18 @@ class CRI_network:
 
         """
         if (target == "simpleSim"):
-            _simple_sim.simulate(neuronModel,threshold,self.axons,self.connections,self.inputs)
+            if(not self.simpleSim):
+                _simple_sim.simple_sim(self.neuronModel, self.threshold, self.axons, self.connections, self.inputs)
+
+            self.simpleSim.free_run()
         else:
             raise Exception("Invalid Target")
 
     def step(self,target="simpleSim"):
         if (target == "simpleSim"):
-            _simple_sim.simulate(neuronModel,threshold,self.axons,self.connections,self.inputs)
+            if(not self.simpleSim):
+                _simple_sim.simple_sim(self.neuronModel, self.threshold, self.axons, self.connections, self.inputs)
+
+            _simple_sim.step_run()
         else:
             raise Exception("Invalid Target")
