d329765cde758f1ba56053c2469315b752faa08c
Justin Frank <justin@pop-os.localdomain>
2022-04-18 10:14:50 -0700

    CRI network class now takes config library as input and target is now a class attribute instead of a method attribute

--- api.py
+++ api.py
@@ -2,34 +2,36 @@ import _simple_sim
 
 class CRI_network:
 
-    def __init__(self,axons,connections,inputs,neuronModel,threshold):
+    def __init__(self,axons,connections,inputs,config,target = 'simpleSim'):
         self.axons = axons
         self.connections = connections
         self.inputs = inputs
-        self.neuronModel = _simple_sim.map_neuron_type_to_int(neuronModel)
-        self.threshold = threshold
+        self.config = config
+        #self.neuronModel = _simple_sim.map_neuron_type_to_int(neuronModel)
+        #self.threshold = threshold
         self.simpleSim = None
+        self.target = target
         #we may need more attributes to track hardware implementation
 
     def update_synapse(self):
         pass
 
-    def freeRun(self,target="simpleSim"):
+    def freeRun(self):
         """Run Network countinuously
 
         """
-        if (target == "simpleSim"):
+        if (self.target == "simpleSim"):
             if(not self.simpleSim):
-                _simple_sim.simple_sim(self.neuronModel, self.threshold, self.axons, self.connections, self.inputs)
+                _simple_sim.simple_sim(_simple_sim.map_neuron_type_to_int(self.config['neuronModel']), self.config['threshold'], self.axons, self.connections, self.inputs)
 
             self.simpleSim.free_run()
         else:
             raise Exception("Invalid Target")
 
     def step(self,target="simpleSim"):
-        if (target == "simpleSim"):
+        if (self.target == "simpleSim"):
             if(not self.simpleSim):
-                _simple_sim.simple_sim(self.neuronModel, self.threshold, self.axons, self.connections, self.inputs)
+                _simple_sim.simple_sim(_simple_sim.map_neuron_type_to_int(self.config['neuronModel']), self.config['threshold'], self.axons, self.connections, self.inputs)
 
             _simple_sim.step_run()
         else:
