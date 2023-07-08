309e77bd1444042ef37450fe72a61252559e3c0e
Gwenevere Frank <justin@pop-os.localdomain>
2023-07-08 11:14:32 -0700

    removed extraneous breakpoint

--- hs_api/_simple_sim.py
+++ hs_api/_simple_sim.py
@@ -437,7 +437,6 @@ class simple_sim:
             if self.perturbMag != None:
                 perturbBits = 17
                 perturbation = Fxp(np.random.randint(-1*2**(perturbBits-1),2**(perturbBits-1),size=nNeurons),dtype=self.formatDict['membrane_potential']) #upper is exclusive so no need to subtract one
-                breakpoint()
                 perturbation( perturbation | Fxp(1,dtype='fxp-u35/0') )#set LSB to 1
                 shift = self.perturbMag - (perturbBits - 1)
                 if shift > 0:
