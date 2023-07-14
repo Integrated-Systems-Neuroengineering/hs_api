dd64fba150bd9af71a96b0791ba8bc585d2869d2
gwenevere <jfrank@ucsd.edu>
2023-07-13 17:18:48 -0700

    working for stoch and leak

--- examples/cmrr_demo.py
+++ examples/cmrr_demo.py
@@ -1,12 +1,12 @@
-from l2s.api import CRI_network
+from hs_api.api import CRI_network
 import sys
 import subprocess
 import time
 #Define a configuration dictionary
 config = {}
-config['neuron_type'] = "I&F"
+config['neuron_type'] = "LI&F"
 config['global_neuron_params'] = {}
-config['global_neuron_params']['v_thr'] = 9
+config['global_neuron_params']['v_thr'] = 2**19
 
 ############################
 # Let's try a simple network
@@ -44,11 +44,11 @@ connections = {'01': [],
                '14': [],
                '15': [],
                '16': [],
-               'a': [('z',4)],
-               'b': [('y',4)],
-               'c': [('x',4)],
-               'd': [('w',4)],
-               'e': [('v',4)],
+               'a': [],
+               'b': [],
+               'c': [],
+               'd': [],
+               'e': [],
                'f': [],
                'g': [],
                'h': [],
@@ -72,36 +72,43 @@ connections = {'01': [],
                'z': []}
 
 #Initialize a CRI_network object for interacting with the hardware and the software
-hardwareNetwork = CRI_network(axons=axons,connections=connections,config=config,target='CRI', outputs = connections.keys())
+hardwareNetwork = CRI_network(axons=axons,connections=connections,config=config,target='CRI', outputs = connections.keys(),perturbMag=30, leak=2**5,simDump = False)
+#hardwareNetwork = CRI_network(axons=axons,connections=connections,config=config,target='CRI', outputs = connections.keys())
+
 # hardwareNetwork = CRI_network(axons=axons,connections=connections,config=config, target='CRI')
-softwareNetwork = CRI_network(axons=axons,connections=connections,config=config, outputs = connections.keys(), target='simpleSim')
+#softwareNetwork = CRI_network(axons=axons,connections=connections,config=config, outputs = connections.keys(), target='simpleSim')
 # softwareNetwork = CRI_network(axons=axons,connections=connections,config=config, target='simpleSim')
 
-hardwareNetwork.write_synapse('alpha', 'a', -3)
-softwareNetwork.write_synapse('alpha', 'a', -3)
+#hardwareNetwork.write_synapse('alpha', 'a', -3)
+#softwareNetwork.write_synapse('alpha', 'a', -3)
 
 #Execute the network stepwise in the hardware and the simulator
-for i in range(100):
-    start = time.time()
-    hwResult, hwSpike = hardwareNetwork.step(inputs[i], membranePotential=True)
-    end = time.time()
+for i in range(20):
+    #start = time.time()
+    if i == 10:
+        print('update pertMag')
+        breakpoint()
+        hardwareNetwork.set_perturbMag(18)
+    hwResult = hardwareNetwork.step(['alpha'],membranePotential = True)
+    #print(inputs[i])
+    #end = time.time()
     #print(end - start)
-    start = time.time()
-    swResult, swSpike = softwareNetwork.step(inputs[i], membranePotential=True)
-    end = time.time()
+    #start = time.time()
+    #swResult, swSpike = softwareNetwork.step(inputs[i], membranePotential=True)
+    #end = time.time()
     #print(end - start)
-    print("timestep: "+str(i)+":")
+    #print("timestep: "+str(i)+":")
     print("hardware result: ")
     #print(synthSpike)
-    print(hwSpike)
+    #print(hwSpike)
     print(hwResult)
-    print("software result: ")
-    print(swSpike)
-    print(swResult)
+    #print("software result: ")
+    #print(swSpike)
+    #print(swResult)
     #Verify that the outputs match
     #for idx in range(len(swResult)):
     #    if(swResult[idx][1] != hwResult[idx][1][3]):
     #        print("Error: potential mismatch! sim: "+str(swResult[idx])+", hw: "+str(hwResult[idx]))
-
+#hardwareNetwork.sim_flush('jul13.txt')    
 
     
