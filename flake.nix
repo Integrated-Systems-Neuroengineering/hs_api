44a4bbfc67bb6aff34082adf9dceb2dbdf0240fa
Gwenevere Frank <gwen@crisdsc0.ucsd.edu>
2026-03-09 11:55:14 -0700

    fix sphinx overlay with proper python311Packages update

--- flake.nix
+++ flake.nix
@@ -36,9 +36,12 @@
           overlays = [
             devshell.overlays.default
             (final: prev: {
-              python311Packages = prev.python311Packages // {
-                sphinx = prev.python311Packages.sphinx.overridePythonAttrs (_: { disabled = false; });
+              python311 = prev.python311.override {
+                packageOverrides = pyFinal: pyPrev: {
+                  sphinx = pyPrev.sphinx.overridePythonAttrs (_: { disabled = false; });
+                };
               };
+              python311Packages = final.python311.pkgs;
             })
           ];
         };
